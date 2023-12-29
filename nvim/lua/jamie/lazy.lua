f8c3d97c6bcc3b9697ae2275c9a6d052827f5d13
Jamie Place <jamieplace95@gmail.com>
2023-12-29 03:23:52 +0000

    made better

--- nvim/lua/jamie/lazy.lua
+++ nvim/lua/jamie/lazy.lua
@@ -77,6 +77,15 @@ require("lazy").setup({
             -- or leave it empty to use the default settings
             -- refer to the configuration section below
           }
+        },
+        {"eandrju/cellular-automaton.nvim"},
+        { "folke/trouble.nvim",
+            dependencies = { "nvim-tree/nvim-web-devicons" },
+            opts = {
+             -- your configuration comes here
+             -- or leave it empty to use the default settings
+             -- refer to the configuration section below
+            },
         }
 })
 
