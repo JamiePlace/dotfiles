e2718a350d2f464053aa2d93ae8d69db78312a21
Jamie Place <jamieplace95@gmail.com>
2024-01-12 03:43:43 +0000

    fixes to lazy

--- nvim/lua/jamie/lazy.lua
+++ nvim/lua/jamie/lazy.lua
@@ -14,80 +14,90 @@ require("lazy").setup({
     -- theme
     {"ellisonleao/gruvbox.nvim" },
     -- treesitter
-    {"nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-        local configs = require("nvim-treesitter.configs")
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function ()
+            local configs = require("nvim-treesitter.configs")
 
-        configs.setup({
-            ensure_installed = {  "lua", "vim", "python", "r", "rust" },
-            sync_install = false,
-            highlight = { enable = true , additional_vim_regex_highlighting = false},
-        })
-    end
+            configs.setup({
+                ensure_installed = {  "lua", "vim", "python", "r", "rust" },
+                sync_install = false,
+                highlight = { enable = true , additional_vim_regex_highlighting = false},
+            })
+        end
     },
     -- lsp
-    {'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
-    dependencies = {
-        -- LSP Support
-        {'neovim/nvim-lspconfig'},
-        {'williamboman/mason.nvim'},
-        {'williamboman/mason-lspconfig.nvim'},
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        dependencies = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
 
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},
-        {'hrsh7th/cmp-buffer'},
-        {'hrsh7th/cmp-path'},
-        {'saadparwaiz1/cmp_luasnip'},
-        {'hrsh7th/cmp-nvim-lsp'},
-        {'hrsh7th/cmp-nvim-lua'},
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
+            {'saadparwaiz1/cmp_luasnip'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-nvim-lua'},
 
-        -- Snippets
-        {'L3MON4D3/LuaSnip'},
-        {'rafamadriz/friendly-snippets'},
-    }
-        },
-        { "williamboman/mason.nvim" },
-        -- fugitive "git"
-        {"tpope/vim-fugitive"},
-        -- telescope "fuzzy finder"
-        {
-            'nvim-telescope/telescope.nvim', tag = '0.1.5',
-            -- or                              , branch = '0.1.x',
-            dependencies = { 'nvim-lua/plenary.nvim' }
-        },
-        --{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
-        -- autopairs
-        {
-            "windwp/nvim-autopairs", event="InsertEnter", opts = {},
-            config = function()
-                require('nvim-autopairs').setup({ })
-            end
-        },
-        -- copilot
-        {"github/copilot.vim"},
-        -- zenmode
-        {
-          "folke/zen-mode.nvim",
-          opts = {
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},
+            {'rafamadriz/friendly-snippets'},
+        }
+    },
+    { "williamboman/mason.nvim" },
+    -- fugitive "git"
+    {"tpope/vim-fugitive"},
+    -- telescope "fuzzy finder"
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        -- or                              , branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    --{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
+    -- autopairs
+    {
+        "windwp/nvim-autopairs", event="InsertEnter", opts = {},
+        config = function()
+            require('nvim-autopairs').setup({ })
+        end
+    },
+    -- copilot
+    {"github/copilot.vim"},
+    -- zenmode
+    {
+        "folke/zen-mode.nvim",
+        opts = {
             -- your configuration comes here
             -- or leave it empty to use the default settings
             -- refer to the configuration section below
-          }
+        }
+    },
+    {"eandrju/cellular-automaton.nvim"},
+    {
+        "folke/trouble.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
         },
-        {"eandrju/cellular-automaton.nvim"},
-        { "folke/trouble.nvim",
-            dependencies = { "nvim-tree/nvim-web-devicons" },
-            opts = {
-             -- your configuration comes here
-             -- or leave it empty to use the default settings
-             -- refer to the configuration section below
-            },
-        },
-        -- dap
-        { "rcarriga/nvim-dap-ui", dependencies = {"mfussenegger/nvim-dap", 'mfussenegger/nvim-dap-python'},{"LiadOz/nvim-dap-repl-highlights"}},
-        { "folke/neodev.nvim", opts = {} }
+    },
+    -- dap
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+            'mfussenegger/nvim-dap-python',
+            "LiadOz/nvim-dap-repl-highlights"
+        }
+    },
+    { "folke/neodev.nvim", opts = {} }
 })
 
 
