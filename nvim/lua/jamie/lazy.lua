4107ae68913dfe7b5fb6b57eec29bfb4257350fc
Jamie Place <jamieplace95@gmail.com>
2024-03-01 09:23:37 +0000

    something

--- nvim/lua/jamie/lazy.lua
+++ nvim/lua/jamie/lazy.lua
@@ -23,7 +23,7 @@ require("lazy").setup({
             local configs = require("nvim-treesitter.configs")
 
             configs.setup({
-                ensure_installed = {  "lua", "vim", "python", "r", "rust" },
+                ensure_installed = {  "lua", "vim", "python", "rust" },
                 sync_install = false,
                 highlight = { enable = true , additional_vim_regex_highlighting = false},
                 indent = { enable = true },
