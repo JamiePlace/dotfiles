3ded4efcb6f582ee4ffd8dae917273af3a0d602e
JamiePlace <jamieplace95@gmail.com>
2024-03-11 16:30:35 +0000

    fixes to telescope for searching

--- nvim/lua/jamie/telescope.lua
+++ nvim/lua/jamie/telescope.lua
@@ -1,14 +1,16 @@
 local telescope = require('telescope')
 
 telescope.setup {
-
+    defaults = {
+        file_ignore_patterns = {
+            ".venv/*",
+            ".git/*",
+            "__pycache__/*"
+        },
+    },
     pickers = {
-
         find_files = {
-
             hidden = true
-
         }
-
     }
 }
