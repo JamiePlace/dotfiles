d0d294f5486c3b2032309e8ccfaff4c5b0bf4087
JamiePlace <jamieplace95@gmail.com>
2024-04-08 17:36:57 +0100

    added todo comment hilighter

--- nvim/lua/jamie/remap.lua
+++ nvim/lua/jamie/remap.lua
@@ -66,4 +66,7 @@ vim.keymap.set('n', "<leader>lr", ":LspRestart<CR>")
 -- docstrings
 -- Generate comment for current line
 vim.keymap.set('n', '<Leader>d', '<Plug>(doge-generate)')
+-- TODO comments
+vim.keymap.set("n", "]t", function() require("todo-comments").jump_next() end, { desc = "Next todo comment" })
 
+vim.keymap.set("n", "[t", function() require("todo-comments").jump_prev() end, { desc = "Previous todo comment" })
