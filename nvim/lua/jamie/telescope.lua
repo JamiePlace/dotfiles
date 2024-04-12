50d2ef1d143401f76a0feb3ca3a23f14ec13f315
Jamie Place <jamieplace95@gmail.com>
2024-04-12 17:10:50 +0100

    added unix check for telescope fzf

--- nvim/lua/jamie/telescope.lua
+++ nvim/lua/jamie/telescope.lua
@@ -14,6 +14,16 @@ telescope.setup {
         }
     }
 }
--- To get fzf loaded and working with telescope, you need to call
--- load_extension, somewhere after setup function:
-telescope.load_extension('fzf')
+function OnUnix()
+    if (package.config:sub(1,1)) == ("\\") then
+        print('on unix')
+        return false
+    end
+    return true
+
+end
+if OnUnix() then
+	-- To get fzf loaded and working with telescope, you need to call
+	-- load_extension, somewhere after setup function:
+	telescope.load_extension('fzf')
+end
