c77c3899a63167e1f639a3403c8698c6b016f067
JamiePlace <jamieplace95@gmail.com>
2024-04-23 16:59:14 +0100

    went mad and added catppuccin theme

--- nvim/lua/jamie/colours.lua
+++ nvim/lua/jamie/colours.lua
@@ -1,8 +1,8 @@
-vim.o.background = "dark"
-require("vscode").setup({})
+vim.o.background = "light"
+--require("vscode").setup({})
 function ColorMyPencils()
     --vim.cmd([[colorscheme gruvbox]])
-    vim.cmd([[colorscheme vscode]])
+    vim.cmd([[colorscheme catppuccin]])
 --    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 --    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
