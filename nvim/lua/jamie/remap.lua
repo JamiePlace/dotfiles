86b57a507742f0f8027d388cdd6936674d103a89
JamiePlace <jamieplace95@gmail.com>
2024-04-30 17:10:34 +0100

    random changes

--- nvim/lua/jamie/remap.lua
+++ nvim/lua/jamie/remap.lua
@@ -11,7 +11,7 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 --paste and copy from clipboard
 -- greatest remap ever
-vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("x", "<leader>pl", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
 vim.keymap.set({"n", "v"}, "<leader>P", [["+p]])
