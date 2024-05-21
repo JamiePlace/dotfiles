faaf24b10118ad9112dd685e494b069b85869dae
JamiePlace <jamieplace95@gmail.com>
2024-05-21 09:24:59 +0100

    added i python config

---
+++ ipython/profile_default/ipython_config.py
@@ -0,0 +1,10 @@
+try:
+    from Ipython import get_config
+except ModuleNotFoundError:
+    pass
+
+c = get_config()  #noqa
+
+c.InteractiveShellApp.exec_lines = []
+c.InteractiveShellApp.exec_lines.append('%load_ext autoreload')
+c.InteractiveShellApp.exec_lines.append('%autoreload 2')
