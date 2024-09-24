76b18523457c6d6ecee96caec60aa59cce04f7ac
JamiePlace <jamieplace95@gmail.com>
2024-09-24 09:59:31 +0100

    loads of cool changes

--- nvim/lua/jamie/lspconfig.lua
+++ nvim/lua/jamie/lspconfig.lua
@@ -56,7 +56,7 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
   vim.keymap.set("n", "<leader>fm", function() Format() end, opts)
-  vim.keymap.set("n", "<leader>ff", function() vim.lsp.buf.formatting() end, opts)
+  vim.keymap.set("n", "<leader>bf", function() vim.lsp.buf.formatting() end, opts)
   vim.keymap.set("n", "<leader>lr", ":LspRestart<CR>")
 
 end)
