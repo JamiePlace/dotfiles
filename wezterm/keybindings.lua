56160472947c69a3e66f05b4e1fdd8163c62ffa2
Jamie Place <jamieplace95@gmail.com>
2024-10-22 02:33:20 +0100

    fixes

--- wezterm/keybindings.lua
+++ wezterm/keybindings.lua
@@ -157,5 +157,49 @@ local keys = {
             )
         end)
     },
+    {
+        key = 'T',
+        mods = 'ALT|SHIFT',
+        action = wezterm.action_callback(function(window, pane)
+            -- get builting color schemes
+            local schemes = wezterm.get_builtin_color_schemes()
+            local choices = {}
+
+            -- populate theme names in choices list
+            for key, _ in pairs(schemes) do
+                table.insert(choices, { label = tostring(key) })
+            end
+
+            -- sort choices list
+            table.sort(choices, function(c1, c2)
+                return c1.label < c2.label
+            end)
+
+            window:perform_action(
+                act.InputSelector({
+                    title = "🎨 Pick a Theme!",
+                    choices = choices,
+                    fuzzy = true,
+
+                    -- execute 'sed' shell command to replace the line 
+                    -- responsible of colorscheme in my config
+                    action = wezterm.action_callback(function(inner_window, inner_pane, _, label)
+                        inner_window:perform_action(
+                            act.SpawnCommandInNewTab({
+                                args = {
+                                    "sed",
+                                    "-i",
+                                    's/config.color_scheme =.*/config.color_scheme = "' .. label .. '"/',
+                                    "/mnt/c/Users/jamie/.config/wezterm/wezterm.lua",
+                                },
+                            }),
+                            inner_pane
+                        )
+                    end),
+                }),
+                pane
+            )
+        end)
+    },
 }
 return keys
