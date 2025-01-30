8ad3f27ca4c4f8f77c7a4537daffb54f896cfd1c
Jamie Place <jamieplace95@gmail.com>
2025-01-30 01:17:05 +0000

    changes

--- wezterm/project_workspaces.lua
+++ wezterm/project_workspaces.lua
@@ -11,7 +11,11 @@ end
 local
 function project_workspace(name, dir, loc)
     local base_dir = "/projects/"
-    local project_dir = wezterm.home_dir .. base_dir .. dir
+    if loc == 'work' then
+        local project_dir = wezterm.home_dir .. base_dir .. dir
+    else
+        local project_dir = "/home/jamie" .. base_dir .. dir
+    end
     local tab, build_pane, window = mux.spawn_window {
         workspace = name,
         cwd = project_dir,
@@ -21,8 +25,14 @@ end
 
 -- generates works spaces for work computer
 function workspaces.work()
+    print('Generating work workspaces')
     for dir in io.popen([[find $HOME/projects/ -maxdepth 2 -type f -name 'pyproject.toml' | xargs -I{} dirname {} | xargs -I{} basename {}]]):lines() do
-        project_workspace(dir, dir, 'work')
+        print(dir)
+        if not tools.on_unix() then
+            project_workspace(dir, dir, 'home')
+        else
+            project_workspace(dir, dir, 'work')
+        end
     end
 end
 
