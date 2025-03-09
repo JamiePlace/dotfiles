5512f59f7f3edd699fbeac9907181ec2aade9bb5
Jamie Place <jamieplace95@gmail.com>
2025-03-09 14:37:13 +0000

    remove powershell nonsense

--- nvim/lua/jamie/init.lua
+++ nvim/lua/jamie/init.lua
@@ -16,15 +16,6 @@ require("jamie.oil")
 require("jamie.neotest")
 
 
-if vim.fn.has('macunix') == 0 then
-    vim.cmd([[
-		let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
-		let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';'
-		let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
-		let &shellpipe  = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
-		set shellquote= shellxquote=
-    ]])
-end
 -- vim.g.netrw_browse_split = 0
 -- vim.g.netrw_banner = 0
 -- vim.g.netrw_winsize = 25
