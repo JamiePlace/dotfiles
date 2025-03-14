2dcd360ae0c6373945fa4422fc35ffe6cd26c0eb
Jamie Place <jamieplace@Jamies-MacBook-Air.local>
2025-03-14 20:21:17 +0000

    changed to vscode theme

--- nvim/lua/jamie/colours.lua
+++ nvim/lua/jamie/colours.lua
@@ -1,9 +1,9 @@
 --vim.o.background = "light"
 vim.o.background = "dark"
---require("vscode").setup({})
+require("vscode").setup({})
 function ColorMyPencils()
     --vim.cmd([[colorscheme gruvbox]])
-    vim.cmd([[colorscheme rose-pine]])
+    vim.cmd([[colorscheme vscode]])
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     --vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
